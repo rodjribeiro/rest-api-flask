ea92a0bada091c762f2d865b4e74f94e8f6d8638
rodjribeiro <rodrigojdr13@gmail.com>
2024-08-18 21:50:06 -0300

    feat: refactor app file

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    "db": "users",
+    "host": "mongodb",
+    "port": 27017,
+    "username": "admin",
+    "password": "admin"
+}
 api = Api(app)
+db = MongoEngine(app)
 
 
-class HelloWorld(Resource):
+class User(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.EmailField(required=True)
+    birth_date = db.DateTimeField(required=True)
+
+
+class Users(Resource):
     def get(self):
-        print('oi')
-        return {'hello': 'world'}
+        return jsonify(User.objects())
 
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(Users, '/')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
